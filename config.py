a7a20c67bc3c31468741aad2b1700d5750800021
msNPS <adgrom@mail.ru>
2023-04-22 21:00:59 +0300

    Added config gile

---
+++ config.py
@@ -0,0 +1,6 @@
+WINDOW_SIZE = "1000x800"
+BUTTON_COLOR = ("#68b885", "#31604e")
+TEXT_COLOR = ("gray10", "gray90")
+HOVER_COLOR = ("gray70", "gray30")
+OPTION_COLOR = ("gray70", "gray30")
+OPTION_HOVER_COLOR = ("gray80", "gray20")
