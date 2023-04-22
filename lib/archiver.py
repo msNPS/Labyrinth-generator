7268652a18420eb2a636da7351b9039651fdb18e
msNPS <adgrom@mail.ru>
2023-04-22 14:02:14 +0300

    Archiver now can export as png

--- lib/archiver.py
+++ lib/archiver.py
@@ -1,16 +1,24 @@
 import struct
+import cv2
 
 
 def import_maze(filename):
+    algorithm_dict = {0: "DFS", 1: "BFS", 2: "Kruskal's"}
     with open(filename, "rb") as f:
         width = struct.unpack("<H", f.read(2))[0]
         height = struct.unpack("<H", f.read(2))[0]
+        algorithm = struct.unpack("<H", f.read(2))[0]
         seed = struct.unpack("<d", f.read(8))[0]
-    return width, height, seed
+    return width, height, algorithm_dict[algorithm], seed
 
 
 def export_maze(maze, filename):
-    with open(filename + ".maze", "wb") as f:
-        f.write(struct.pack("<H", maze.width))
-        f.write(struct.pack("<H", maze.height))
-        f.write(struct.pack("<d", maze.seed))
+    if filename.endswith(".maze"):
+        algorithm_dict = {"DFS": 0, "BFS": 1, "Kruskal's": 2}
+        with open(filename, "wb") as f:
+            f.write(struct.pack("<H", maze.width))
+            f.write(struct.pack("<H", maze.height))
+            f.write(struct.pack("<H", algorithm_dict[maze.algorithm]))
+            f.write(struct.pack("<d", maze.seed))
+    elif filename.endswith(".png"):
+        cv2.imwrite(filename, maze.resize(maze.last_draw))
